5fa2ad76cfb46bae2523cfeaea3a1d9133c90af0
hardik yadav <hardikyadav1182005@gmail.com>
2024-03-09 23:56:19 +0530

    commit

--- http requext/index.js
+++ http requext/index.js
@@ -2,8 +2,26 @@ import express from "express";
 const app= express();
 
 app.get("/", (req,res) =>{
-    console.log("hello world this is live");
+    //console.log(req.rawHeaders);
+    res.send("<h3> hello world </h3>");
 });
+app.post("/register", (req,res) =>{
+    //console.log(req.rawHeaders);
+    res.sendStatus(201);
+});
+app.put("/user/hardik", (req,res) =>{
+    //console.log(req.rawHeaders);
+    res.sendStatus(200);
+});
+app.patch("/user/hardik", (req,res) =>{
+    //console.log(req.rawHeaders);
+    res.sendStatus(200);
+});
+app.delete("/user/hardik", (req,res) =>{
+    //console.log(req.rawHeaders);
+    res.sendStatus(200);
+});
+
 
 app.listen(8000,() =>{
     console.log("hello how are you doing");
